3bf267b9cfe1f084af8d011877c880e02b70062c
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-12-30 20:48:53 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+
+# Load data from categorized_stocks.xlsx
+data_path = "categorized_stocks.xlsx"
+df = pd.read_excel(data_path)
+
+# Sidebar for user input
+st.sidebar.header("Search Stocks")
+search_query = st.sidebar.text_input("Enter stock name:")
+
+# Filter data based on user input
+filtered_df = df[df['Stock'].str.contains(search_query, case=False)]
+
+# Display results
+if not filtered_df.empty:
+    st.subheader("Search Results")
+    st.table(filtered_df[['Stock', 'Volatility', 'Beta', 'Return_on_Investment', 'Debt_to_Equity_Ratio', 'Category']])
+else:
+    st.warning("No results found.")
+
+# Additional information
+st.sidebar.markdown("---")
+st.sidebar.markdown("### Additional Information")
+st.sidebar.write("This app fetches data from categorized_stocks.xlsx and allows you to search for stocks.")
+st.sidebar.write("Columns displayed: Volatility, Beta, Return_on_Investment, Debt_to_Equity_Ratio, Category.")
